d62878bf90d1d54204a0b3534e139617bcce8e43
davidmakarovv <davidmakarr@gmail.com>
2024-01-25 18:47:10 +0400

    bg photo added & styled with captions

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const hamMenu = document.querySelector(".ham-menu");
 const burgerMenu = document.querySelector(".burger-menu");
 const overlay = document.querySelector(".overlay");
 
-hamMenu.addEventListener("click", () => {
+const burgerMenuToggle = () => {
   hamMenu.classList.toggle("active");
   burgerMenu.classList.toggle("active");
   overlay.classList.toggle("hidden");
-});
+};
 
-overlay.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  burgerMenu.classList.toggle("active");
-  overlay.classList.toggle("hidden");
-});
+hamMenu.addEventListener("click", burgerMenuToggle);
+
+overlay.addEventListener("click", burgerMenuToggle);
